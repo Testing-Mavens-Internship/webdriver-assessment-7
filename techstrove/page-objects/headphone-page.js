c5b12a1376fc326ed6bf117adefc9817f5751a6e
jesson006 <jessonjose12@gmail.com>
2023-09-25 16:31:15 +0530

    Assesment Modified

--- techstrove/page-objects/headphone-page.js
+++ techstrove/page-objects/headphone-page.js
@@ -1,17 +1,18 @@
+const Common = require('./common.js');
 class HeadphonePage {
     constructor() {
         this.$viewHeadPhone = () => $selectHeadPhone(`(//div[contains(text(),"P9 Air Max Wireless Stereo Headphone")])[1]`);
         this.$verifyProductAndPrice = (name, price) => $verifyPrice(`//div[contains(text(),"${name}")]/..//div[contains(text(),"${price}")]`);
         this.$productHeader = () => $productHeader(`//h1[contains(text(),"P9 Air")]`);
         this.$selectColorDropdown = () => $selectColorDropdown(`//select[@data-index="option1"]`);
-        this.$selectColor = colorName => $selectColor(`//option[@name="Color" and text()="${colorName}"]`);
-        this.$productNumber = plus => $productNumber(`(//span[text()="${plus}"])[1]`);
+        this.$selectColor = (colorName) => $selectColor(`//option[@name="Color" and text()="${colorName}"]`);
+        this.$productNumber = (plus) => $productNumber(`(//span[text()="${plus}"])[1]`);
         this.$addToCart = () => $addToCart(`//span[@id="AddToCartText-8378768654650"]`);
         this.$cartHeader = () => $cartHeader(`//div[text()="Cart"]`);
     }
 
     /**
-     *  select a headphone and view it
+     *  select a headphone and view the headphone
      */
     async viewHeadphone() {
             await this.$viewHeadPhone().click();
--- techstrove/page-objects/home-page.js
+++ techstrove/page-objects/home-page.js
@@ -2,10 +2,13 @@ const Common = require('./common.js');
 
 class Homepage extends Common {
     constructor() {
-        super();
-        this.$colloection = () => $colloection(`//li[contains(@class,"site-nav__item site-nav__expanded-item s")]`);
-        this.$headPhones = () => this.$headPhones(`(//ul[@class="site-nav__dropdown text-left"]//child::a)[1]`);
-    }
+            super();
+            this.$colloection = () => $colloection(`//li[contains(@class,"site-nav__item site-nav__expanded-item s")]`);
+            this.$headPhones = () => this.$headPhones(`(//ul[@class="site-nav__dropdown text-left"]//child::a)[1]`);
+        }
+        /**
+         * Hover Over the collection elemnt in the page and click on headphone
+         */
     async hoverOverTheCollection() {
         await this.$colloection().moveTo();
         await this.$headPhones().click();
